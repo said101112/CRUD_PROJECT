600742b92dcadfdc4741eb4cc34e917658fafae1
nichan said <said.nichan14@gmail.com>
2026-04-10 20:34:36 +0100

    chore: ajouter env variables et gitignore au backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'project_management'
 });
 
